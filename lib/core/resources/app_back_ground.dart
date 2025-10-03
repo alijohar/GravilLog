483e5f9dd50e64bcc68c4b2c079c49809c9a556a
ranamohyy <ranaamr22@gmail.com>
2025-10-03 23:07:31 +0300

    make class for background to reduce repeating code

---
+++ lib/core/resources/app_back_ground.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'color_manager.dart';
+class AppBackGround extends StatelessWidget {
+  const AppBackGround({super.key,required this.child});
+final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+    width: double.infinity,
+    height: double.infinity,
+    decoration: const BoxDecoration(
+    gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [ColorManager.peachyPink, ColorManager.skyBlue])),
+    child: child
+    );
+
+  }
+}
--- lib/featuers/splash/presentation/pages/splash_screen.dart
+++ lib/featuers/splash/presentation/pages/splash_screen.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gravilog_2025/core/resources/app_spaces.dart';
 import 'package:gravilog_2025/core/resources/assets_manager.dart';
-import 'package:gravilog_2025/core/resources/color_manager.dart';
 import 'package:gravilog_2025/featuers/splash/presentation/controllers/splash_controller.dart';
+
+import '../../../../core/resources/app_back_ground.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
@@ -11,14 +12,7 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [ColorManager.peachyPink, ColorManager.skyBlue])),
+      body:AppBackGround(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
