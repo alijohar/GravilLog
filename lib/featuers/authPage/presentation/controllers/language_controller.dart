b73a09d5d2086a6d556cd03396d73f7f82d99aac
ranamohyy <ranaamr22@gmail.com>
2025-10-04 02:48:16 +0300

    refactor language controller

--- lib/featuers/authPage/presentation/controllers/language_controller.dart
+++ lib/featuers/authPage/presentation/controllers/language_controller.dart
@@ -1,50 +1,39 @@
+import 'dart:developer';
 import 'dart:ui';
-
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../../../core/errors/failure.dart';
 import '../../../../core/local_preferences/local_preferences.dart';
 import '../../../../core/resources/routes_manager.dart';
-import '../../business/entities/auth_result_entity.dart';
 
 class LanguageController extends GetxController {
-   LocalPreferences localDataSource = Get.find();
-   var currentLanguage = "en".obs;
+  LocalPreferences localDataSource = Get.find();
+  var currentLanguage = "en".obs;
 
-
-  Future<void> onInit() async {
+  @override
+  void onInit() async {
     super.onInit();
-    localDataSource = LocalPreferences(
-      await SharedPreferences.getInstance(),
-    );
+    localDataSource = LocalPreferences(await SharedPreferences.getInstance());
     currentLanguage.value = getDeviceLanguage();
-    print("currentLanguage ist ${currentLanguage.value}");
+    log("currentLanguage ist ${currentLanguage.value}");
   }
 
   Future<void> onContinue() async {
-
-     await changeLanguage(currentLanguage.value);
-     await setDeviceLanguage(currentLanguage.value);
-     Get.toNamed(Routes.onboardingRoute);
-
+    await changeLanguage(currentLanguage.value);
+    await setDeviceLanguage(currentLanguage.value);
+    Get.toNamed(Routes.onboardingRoute);
   }
 
-    changeLanguage(String languageCode) async{
-   await  Get.updateLocale(Locale(languageCode));
+  Future<void> changeLanguage(String languageCode) async {
+    await Get.updateLocale(Locale(languageCode));
   }
 
-
-
-
-
-    setDeviceLanguage(String languageCode )async{
+  Future<void> setDeviceLanguage(String languageCode) async {
     await localDataSource.setLanguage(languageCode);
   }
-   String getDeviceLanguage(){
-    return  localDataSource.getLanguage()??Get.deviceLocale?.languageCode??'en';
 
+  String getDeviceLanguage() {
+    return localDataSource.getLanguage() ??
+        Get.deviceLocale?.languageCode ??
+        'en';
   }
 }
