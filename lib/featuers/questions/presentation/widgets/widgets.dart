527283990a56b3e81632caa714142c89cc74c947
ranamohyy <ranaamr22@gmail.com>
2025-10-13 06:53:28 +0300

    refactor:export widgets

--- lib/featuers/questions/presentation/widgets/widgets.dart
+++ lib/featuers/questions/presentation/widgets/widgets.dart
@@ -1,1 +1,22 @@
+export '../pages/medical_history_page.dart';
+export  'package:flutter_screenutil/flutter_screenutil.dart';
+export  'package:gravilog_2025/core/resources/app_spaces.dart';
+export  '../../../../core/resources/app_text_styles.dart';
+export  '../widgets/container_with_shadow_color.dart';
+export  'package:flutter/material.dart';
+export  'package:gravilog_2025/core/resources/app_elevated_button.dart';
+export  '../../../../core/resources/color_manager.dart';
+export  'package:flutter_svg/svg.dart';
+export  '../../../../core/resources/assets_manager.dart';
+export  '../pages/pregnancy_info_page.dart';
+export  '../../../../core/resources/app_back_ground.dart';
+export  '../pages/about_you_page.dart';
+
+
+
+
+
+
+
+
 
