b7055818ccedfa3dc548f7d04fb02d59df0a1870
ranamohyy <ranaamr22@gmail.com>
2025-10-13 06:09:31 +0300

    style:edit elevated button

--- lib/core/resources/app_elevated_button.dart
+++ lib/core/resources/app_elevated_button.dart
@@ -1,27 +1,31 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/get_utils.dart';
 import 'package:gravilog_2025/core/resources/app_text_styles.dart';
 
 class AppElevatedButton extends StatelessWidget {
- const AppElevatedButton({super.key, required this.onPressed, required this.text,this.backGroundColor,this.textStyle});
+  const AppElevatedButton(
+      {super.key,
+      required this.onPressed,
+      required this.text,
+      this.backGroundColor,this.elevation,
+      this.textStyle});
   final void Function() onPressed;
   final String text;
-  final Color ?backGroundColor;
-  final TextStyle ?textStyle;
+  final Color? backGroundColor;
+  final TextStyle? textStyle;
+  final double? elevation;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor:
-          backGroundColor??context.theme.primaryColor
-        ),
-        child: Text(
-          text.tr,
-          style:textStyle??AppTextStyles.kTextStyle16ButtonText800,
-        ),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.grey,
+          elevation: elevation??0,
+          backgroundColor: backGroundColor ?? context.theme.primaryColor),
+      child: Text(
+        text.tr,
+        style: textStyle ?? AppTextStyles.kTextStyle16ButtonText800,
+      ),
     );
   }
 }
