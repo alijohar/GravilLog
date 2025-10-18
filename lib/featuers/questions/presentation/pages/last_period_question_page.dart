a657f13d1736bba615a0a3aed7b23a75245dae2e
ranamohyy <ranaamr22@gmail.com>
2025-10-18 22:26:01 +0300

    feature: make last question screen

---
+++ lib/featuers/questions/presentation/pages/last_period_question_page.dart
@@ -0,0 +1,57 @@
+import '../widgets/widgets.dart';
+
+class LastPeriodQuestionPage extends StatelessWidget {
+  const  LastPeriodQuestionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBackGround(
+      child: Scaffold(
+        appBar: AppBar(leading:const BackButton(),),
+        body: Padding(
+          padding:  EdgeInsets.all(16.r),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("last-period".tr,
+                  textAlign: TextAlign.center,
+                  style:AppTextStyles.kTextStyleHead20Weight800
+              ),
+              const HeightSpace(16),
+              const DatesPicker(),
+              const HeightSpace(24),
+              // Button
+              Text(
+                "cycle_length".tr,
+                style: AppTextStyles.kTextStyleHead16Weight700,
+              ),
+              const HeightSpace(8),
+              PeriodDaysDropdownWidget(),
+              const HeightSpace(32),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.r),
+                  color: context.pinkSherbet.withAlpha(40),
+                ),
+                child: Text(
+                    "last-period-sheet".tr,
+                    textAlign: TextAlign.center,
+                    style: AppTextStyles.kTextStyle14MediumGrey400.copyWith(color:ColorManager.primary)
+                ),
+              ),
+              const HeightSpace(24),
+             const Spacer(),
+              AppElevatedButton(
+                  onPressed: () {},
+                  text: "continue"),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
