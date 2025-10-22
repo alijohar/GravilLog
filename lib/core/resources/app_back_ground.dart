c10b7b5dadcbcc3f27b454372524677248f8060d
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-10-22 05:42:18 +0300

    feat: add new background widgets and update color manager with sky light colors

--- lib/core/resources/app_back_ground.dart
+++ lib/core/resources/app_back_ground.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'color_manager.dart';
 
 class AppBackGround extends StatelessWidget {
@@ -14,6 +15,41 @@ class AppBackGround extends StatelessWidget {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [ColorManager.peachyPink, ColorManager.skyBlue],
+          ),
+        ),
+        child: child);
+  }
+}
+
+class AppBackGroundSkyPinkLight extends StatelessWidget {
+  const AppBackGroundSkyPinkLight({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: ColorManager.skyPinkLight
+          
+        ),
+        child: child);
+  }
+}
+
+class AppBackGround2 extends StatelessWidget {
+  const AppBackGround2({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [ColorManager.skyPinkLight, ColorManager.skyBlueLight],
           ),
         ),
         child: child);
